12c214c6ca1def7a775934f17ed29ff36397d504
Anastasia <avkozlova8@gmail.com>
2024-11-01 12:51:12 +0300

    .

--- onehandbandit/Model/Card.swift
+++ onehandbandit/Model/Card.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct Card: Codable, Identifiable {
+    let id: String
+    let icon: String 
+    var iconColor: String
+    var bgColor: String
+}
+
+// расширение для текущей модели, в котором хранятся демо данные
+extension Card {
+    static var MOCK_CARD: [Card] = [
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white"),
+        .init(id: NSUUID().uuidString, icon: "star", iconColor: "orange", bgColor: "white")
+    ]
+}
